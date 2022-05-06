78fb4a86ce3f5d12daaea91285765cca0e123c1c
Big Ship <callumormerod85@gmail.com>
2022-05-06 17:32:13 +0100

    wild west

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'E:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'belfast-south'
+constituency = 'belfast-west'
 
 full_path = path_init + constituency + '/'
 
