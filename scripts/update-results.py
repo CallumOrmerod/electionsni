73e83404724ebd74e11b4b60cf9e8859fa393237
Big Ship <callumormerod85@gmail.com>
2022-05-06 17:50:42 +0100

    South Down

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'E:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'mid-ulster'
+constituency = 'south-down'
 
 full_path = path_init + constituency + '/'
 
