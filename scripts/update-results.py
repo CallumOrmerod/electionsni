acf5c4b987d3660fefc245b4ab0f1bacd81f1e1a
Callum <callum.ormerod@aquaq.co.uk>
2022-05-08 21:33:24 +0100

    north down

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'C:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'north-antrim'
+constituency = 'north-down'
 
 full_path = path_init + constituency + '/'
 
