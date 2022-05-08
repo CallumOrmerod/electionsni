e881a6c994367081da3452bbbcaa7fe26216d67f
Callum <callum.ormerod@aquaq.co.uk>
2022-05-08 22:22:20 +0100

    strangford

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'C:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'south-down'
+constituency = 'strangford'
 
 full_path = path_init + constituency + '/'
 
