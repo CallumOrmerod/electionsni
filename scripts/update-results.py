0826d1e4145b133ac33159ba001ca12dddb78124
Callum <callum.ormerod@aquaq.co.uk>
2022-05-08 18:35:28 +0100

    FST

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'C:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'east-londonderry'
+constituency = 'fermanagh-south-tyrone'
 
 full_path = path_init + constituency + '/'
 
