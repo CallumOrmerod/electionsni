79ecb73ca8fc0bd637cd65a8856a37101d7be2e0
Callum <callum.ormerod@aquaq.co.uk>
2022-05-08 14:25:27 +0100

    belfast east

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'C:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'newry-armagh'
+constituency = 'belfast-east'
 
 full_path = path_init + constituency + '/'
 
