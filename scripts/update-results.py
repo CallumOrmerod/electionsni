459300bdfe620b1d4a2b8b14beb4703cacca69c1
Callum <callum.ormerod@aquaq.co.uk>
2022-05-08 15:48:12 +0100

    Belfast North

--- scripts/update-results.py
+++ scripts/update-results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 path_init = 'C:/Personal/Github/electionsni/2022/constituency/'
 
-constituency = 'belfast-east'
+constituency = 'belfast-north'
 
 full_path = path_init + constituency + '/'
 
